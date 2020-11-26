8d5c1dc6490ff55a30b48b430a53a30b84699ba8
DanielMagik <danik98@onet.pl>
2020-11-26 19:24:13 +0100

    Add configuration files and class

---
+++ src/main/java/pl/documents/AppConfiguration.java
@@ -0,0 +1,33 @@
+package pl.documents;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "appconfiguration")
+public class AppConfiguration
+{
+    private int accessBefore;
+    private int deleteAfter;
+
+    public int getAccessBefore()
+    {
+        return accessBefore;
+    }
+
+    public void setAccessBefore(int accessBefore)
+    {
+        this.accessBefore = accessBefore;
+    }
+
+    public int getDeleteAfter()
+    {
+        return deleteAfter;
+    }
+
+    public void setDeleteAfter(int deleteAfter)
+    {
+        this.deleteAfter = deleteAfter;
+    }
+}
